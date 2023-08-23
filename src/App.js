72f9e2c8cf1a4df82ead4d9686319859439b0f82
Prince <prince.pm.mishra@gmail.com>
2023-08-24 02:43:11 +0530

    removed unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import './App.scss';
-import Protected from './Protected';
 import DisplayNotes from './components/DisplayNotes';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import {
-  createBrowserRouter,
-  // createRoutesFromElements,
-  // Route,
-  RouterProvider,
-} from "react-router-dom";
-
 
 function App() {
-  const loggedIn = window.localStorage.getItem("isUserLoggedIn");
-  
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: loggedIn ? <DisplayNotes /> : <Login />,
-  //     // element: <Protected Component={DisplayNotes}/>,
-  //   },
-  //   {
-  //     path: "SignUp",
-  //     // element: <SignUp />,
-  //     element: loggedIn ? <DisplayNotes /> : <SignUp />,
-  //   },
-  //   {
-  //     path: "login",
-  //     // element: <Login />,
-  //     element: loggedIn ? <DisplayNotes /> : <Login />,
-  //   }
-  // ]);
-  
-
   return (
     <div className="app">
-      {/* <RouterProvider router = {router} /> */}
       <DisplayNotes />
     </div>
   );
